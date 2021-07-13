a952564445becb34ae573a0c7b3b59c6353b813a
Viktor Gamov <gamussa@gmail.com>
2021-07-13 11:32:55 -0400

    added gRPC streaming example

--- src/main/java/io/kong/developer/servicereactive/grpc/DuneRpcService.java
+++ src/main/java/io/kong/developer/servicereactive/grpc/DuneRpcService.java
@@ -6,7 +6,7 @@ import com.github.javafaker.Faker;
 
 import net.devh.boot.grpc.server.service.GrpcService;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import java.util.stream.Stream;
 
 import io.grpc.stub.StreamObserver;
 import io.kong.developer.quoteservice.QuoteMessage;
@@ -16,13 +16,21 @@ import lombok.RequiredArgsConstructor;
 @GrpcService
 @RequiredArgsConstructor
 public class DuneRpcService extends QuoteServiceGrpc.QuoteServiceImplBase {
-  
+
   @Override
   public void getQuote(final Empty request,
                        final StreamObserver<QuoteMessage> responseObserver) {
 
     final QuoteMessage message = QuoteMessage.newBuilder().setMessage(Faker.instance().dune().quote()).build();
     responseObserver.onNext(message);
+    responseObserver.onCompleted();
+  }
+
+  @Override
+  public void getQuoteStream(final Empty request, final StreamObserver<QuoteMessage> responseObserver) {
+    Stream.generate(() -> QuoteMessage.newBuilder().setMessage(Faker.instance().dune().quote()).build())
+        .limit(10)
+        .forEach(responseObserver::onNext);
     responseObserver.onCompleted();
   }
 }
